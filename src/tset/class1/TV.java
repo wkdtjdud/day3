3d78a184a0634d40fbc92e060644458b5f6decf9
wkdtj <wkdtj@192.168.219.103>
2022-01-05 14:16:57 +0900

    comple

---
+++ src/tset/class1/TV.java
@@ -0,0 +1,76 @@
+package tset.class1;
+
+public class TV {
+	private String color, producer;
+	private int size, channel, volume;
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public String getColor() { 
+		return color;
+	}
+
+	@Override
+	public String toString() {
+		return "TV [color=" + color + ", producer=" + producer + ", size=" + size + ", channel=" + channel + ", volume="
+				+ volume + "]";
+	}
+
+	public String getProducer() {
+		return producer;
+	}
+
+	public void setProducer(String producer) {
+		this.producer = producer;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public int getChannel() {
+		return channel;
+	}
+
+	public void setChannel(int channel) {
+		this.channel = channel;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+	public void powerOn()
+	{
+		System.out.println(producer+"Tv전원 꺼짐");
+	}
+	public void powerOff()
+	{
+		System.out.println(producer+"Tv전원 켜짐");
+	}
+	public void upChannel() {
+		channel++;
+		System.out.println("현재 채널"+channel+"번");
+	}
+	public void douwnChannel() {
+		channel--;
+		System.out.println("현재 채널"+channel+"번");
+	}
+	public void upVolume() {
+		volume++;
+		System.out.println("현재 볼륨: "+volume);
+	}
+	public void downVolume() {
+		volume--;
+		System.out.println("현재 볼륨: "+volume);
+	}
+}
